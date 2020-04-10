e971eae57b47121e9f61222dcb48a9700b8b3a13
Driss Boumlik <boumlik.t@gmail.com>
2020-04-10 14:59:36 +0100

    Add parallax

---
+++ Parallax-on-the-Web-DevTips--master/START/js/functions.js
@@ -0,0 +1,26 @@
+$(window).scroll(function(){
+  
+  var wScroll = $(this).scrollTop();
+
+  $('.logo').css({
+    'transform': 'translate(0px, '+ wScroll/2 +'%)'
+  })
+
+  $('.back-bird').css({
+    'transform': 'translate(0px, '+ wScroll/4 +'%)'
+  })
+
+  $('.fore-bird').css({
+    'transform': 'translate('+ wScroll/5 +'px, -'+ wScroll/40 +'%)'
+  })
+
+  if(wScroll > $('.clothes-pics').offset().top - ($(window).height() / 1.2)){
+    $('.clothes-pics figure').each(function(i){
+
+      setTimeout(function(){
+        $('.clothes-pics figure').eq(i).addClass('is-showing');
+      }, 150*(i+1));
+    });
+  }
+
+});
